66ce83a1d75d31b6d64b0d29ac169bdd6296f0a6
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-13 14:37:55 -0300

    Defining coupon schema

---
+++ src/models/Coupon.js
@@ -0,0 +1,45 @@
+//  Loading database module
+const mongoose = require("mongoose");
+
+//	Using schema feature from mongoose and calling schemas
+const Schema = mongoose.Schema;
+
+//	Defining coupon schema
+const CouponSchema = Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	user: {
+		type: String,
+    required: true
+  },
+  type: {
+		type: String,
+		required: true
+  },
+  qtd: {
+		type: Number,
+    default: 0
+  },
+  method: {
+		type: String,
+		required: true
+  },
+  discount: {
+		type: Number,
+    default: 10
+  },
+  available: {
+		type: Boolean,
+		default: 0,
+    required: true
+  },
+	creationDate: {
+		type: Date,
+		default: Date.now()
+	}
+});
+
+//	Creating collection Coupon on database
+mongoose.model("Coupon", CouponSchema);
