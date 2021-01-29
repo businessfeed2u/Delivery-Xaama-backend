6d4e65588f0aa0d031d7cefe9bfd1bc2edaec8d8
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-01-29 08:53:58 -0300

    Merge

--- src/helpers/systemOpen.js
+++ src/helpers/systemOpen.js
@@ -13,14 +13,6 @@ exports.systemOpen = (companyInfo) => {
 	const current = new Date("2020-07-28 " + date.getHours() + ":" + date.getMinutes());
 	const open = new Date("2020-07-28 " + openHour);
   const end = new Date("2020-07-28 " + endHour);
-<<<<<<< HEAD
-
-  // console.log("date: " + date);
-  // console.log("current: " + current);
-  // console.log("open: " + open);
-  // console.log("end: " + end);
-=======
->>>>>>> 4536fe70845bb47f860f9c9d2f80d665c3cf5424
 
 	if(end.getTime() < open.getTime()) {
 		if ((current.getTime() >= open.getTime()) || (current.getTime() <= end.getTime())) {
