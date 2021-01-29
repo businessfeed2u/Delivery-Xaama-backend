a72bee84f4b05b9b36db27d5fc074fb8cbe3ca39
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-29 15:08:46 -0300

    att cors

--- src/index.js
+++ src/index.js
@@ -16,10 +16,16 @@ const port = process.env.PORT || 4000;
 const server = http.Server(app);
 setupWebsocket(server);
 
+//  Configuring CORS
+var corsOptions = {
+  origin: process.env.URL,
+  optionsSuccessStatus: 200
+};
+
 //  Must be below middleware
 //  Using modules
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
