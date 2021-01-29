f6f28de2a16e0da3aab6fca87d714e5b45cade68
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-01-29 09:10:52 -0300

    Fixing bug

--- src/helpers/systemOpen.js
+++ src/helpers/systemOpen.js
@@ -12,9 +12,7 @@ exports.systemOpen = (companyInfo) => {
       companyInfo.timetable[date.getDay()].endHour ? companyInfo.timetable[date.getDay()].endHour : "";
 
 	const current = new Date("2020-07-28 " + date.getHours() + ":" + date.getMinutes());
-
 	const open = new Date("2020-07-28 " + openHour);
-
 	const end = new Date("2020-07-28 " + endHour);
 
 	if(end.getTime() < open.getTime()) {
